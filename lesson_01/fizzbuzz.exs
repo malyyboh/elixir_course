5153878eeaa7bc047f6f7f990fdc3fee993794c3
Bohdan Danylko <malyyboh@gmail.com>
2025-08-13 22:35:26 +0200

    Implement fizzbuzz function

--- lesson_01/fizzbuzz.exs
+++ lesson_01/fizzbuzz.exs
@@ -1,7 +1,22 @@
 defmodule FizzBuzz do
   def main do
+    1..100
+    |> Enum.map(&fizzbuzz/1)
+    |> Enum.join(" ")
+    |> IO.puts()
   end
 
-  def fizzbuzz do
+  @spec fizzbuzz(integer()) :: String.t()
+  def fizzbuzz(number) do
+    divisible_by_3 = rem(number, 3) == 0
+
+    divisible_by_5 = rem(number, 5) == 0
+
+    cond do
+      divisible_by_3 and divisible_by_5 -> "FizzBuzz"
+      divisible_by_3 -> "Fizz"
+      divisible_by_5 -> "Buzz"
+      true -> to_string(number)
+    end
   end
 end
